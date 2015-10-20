4b299378ee57e8f23bc09061429bf9c26990f417
Paul Sullivan <Psul87@gmail.com>
2015-10-20 03:53:47 -0400

    no message

---
+++ first person somthing/Assets/scripts/destroyoncontact.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyoncontact : MonoBehaviour {
+
+
+		void OnTriggerEnter(Collider other) 
+		{
+			if (other.tag == "Boundary")
+			{
+				return;
+			}
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+	}
